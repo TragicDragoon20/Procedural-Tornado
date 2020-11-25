78dd6f7e2010d8de1fdcf9e9e5b9eca0227a7427
Callum Metcalfe <callummgamedev@hotmail.com>
2020-11-25 20:33:10 +0000

    - Added a tornado effect

---
+++ Assets/Scripts/MeshGeneration/ProceduralTornado.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProceduralTornado : ProceduralBase
+{
+    [SerializeField]
+    private float height = 1.0f;
+    [SerializeField]
+    private float radius = 1.0f;
+    [SerializeField]
+    private float radiusBottom = 0.5f;
+    [SerializeField]
+    private float radiusTop = 0.5f;
+
+    [Space(10)]
+    [SerializeField]
+    private int heightSegmentCount = 10;
+    [SerializeField]
+    private int radialSegmentCount = 10;
+
+
+    public override Mesh BuildMesh()
+    {
+        MeshBuilder meshBuilder = new MeshBuilder();
+        float heightInc = height / heightSegmentCount;
+
+        for (int i = 0; i <= heightSegmentCount; i++)
+        {
+            Vector3 centrepos = Vector3.up * heightInc * i; 
+            float v = (float)i / heightSegmentCount;
+
+            if (i == 0)
+            {
+                BuildRing(meshBuilder, radialSegmentCount, centrepos, radius + radiusBottom, v, i > 0);
+            }
+
+            if (i > 0 && i != heightSegmentCount)
+            {
+                BuildRing(meshBuilder, radialSegmentCount, centrepos, radius + Random.Range(0.0f, 0.3f), v, i > 0);
+            }
+
+            if (i == heightSegmentCount)
+            {
+                BuildRing(meshBuilder, radialSegmentCount, centrepos, radius + radiusTop, v, i > 0);
+            }
+        }
+
+        return meshBuilder.CreateMesh();
+    }
+}
---
+++ Assets/Scripts/TornadoMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TornadoMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float movementRadius = 30.0f;
+    [SerializeField]
+    private float movementSpeed = 0.5f;
+
+    private Vector3 newPos;
+    private Vector3 originalPos;
+    private bool getNewPos;
+
+    private void Start()
+    {
+        originalPos = this.transform.position;
+        getNewPos = true;
+    }
+
+
+    private void Update()
+    {
+        if (getNewPos)
+        {
+            newPos = new Vector3(originalPos.x + Random.Range(-movementRadius, movementRadius), originalPos.y,
+                originalPos.z + Random.Range(-movementRadius, movementRadius));
+            Debug.Log(newPos);
+            getNewPos = false;
+        }
+        
+        this.transform.position = Vector3.MoveTowards(this.transform.position, newPos, movementSpeed);
+
+        if (Vector3.Distance(this.transform.position, newPos) <= 1.5)
+        {
+            getNewPos = true;
+        }
+
+    }
+}
